e7797f12794b1b711b7f9ea5e11597f0d10903e0
fredrik-sundstrom-nti-johanneberg <fredrik.sundstrom@elev.ga.ntig.se>
2020-02-26 10:48:17 +0100

    Create java.js

---
+++ js/java.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("navigering").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("navigering").style.width = "0%";
+}
+
+/* Kommer inte fungera, detta är det gamla sättet att göra de på. Bättre att använda toggel class som vi gjorde med hamburgarmenyn. */
