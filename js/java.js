32dba128fd6a838fd08891d8b90522226d6c99ba
fredrik-sundstrom-nti-johanneberg <fredrik.sundstrom@elev.ga.ntig.se>
2020-03-11 10:52:08 +0100

    Update java.js

--- js/java.js
+++ js/java.js
@@ -2,3 +2,8 @@ document.getElementById('knapp').addEventListener('click', function () {
     document.getElementById('knapp').classList.toggle("change")
     document.getElementById('navigering').classList.toggle("active")
 });
+
+document.getElementById('immaterialratt').addEventListener('click', function () {
+    document.getElementById('immaterialratt').classList.toggle("change")
+    document.getElementById('immaterialratttext').classList.toggle("active")
+});
